87c6fb78156ba1451d9526f764f4f3db4b5a8fc1
Thibolognaize <thibault.feat@gmail.com>
2025-11-06 03:55:34 +0100

    [ADD]_experience section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Experiences from "./components/Experiences";
 
 export default function App() {
   	return (
-    	<>
+		<>
 			<div className="p-5 md:px-[15%]">
-				<Navbar/>
-				<Home/>
+				<Navbar />
+				<Home />
 			</div>
-			<About/>
+			<About />
+			<div className="p-5 md:px-[15%]">
+				<Experiences />
+			</div>
 		</>
-  	)
+	);
 }
